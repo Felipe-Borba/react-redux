cae4b516fd6a19f36b6ba6c9534533adc166a66d
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-24 20:57:50 -0300

    add json server

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
 
 function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get("trips");
+      setTrips(response.data);
+    }
+
+    loadApi();
+  }, []);
+
   return (
     <div>
       <h1>Home!</h1>
