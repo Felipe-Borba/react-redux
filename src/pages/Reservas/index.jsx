e554c56425952306b2c49ffa05c61f108c16588b
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-24 21:57:05 -0300

    add style to reservas page

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
+import "./style.css";
 
 function Reservas() {
   return (
     <div>
-      <h1>Reservas</h1>
+      <h1 className="title">Reservas</h1>
+
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="teste"
+        />
+
+        <strong>abc</strong>
+
+        <span>Bfd</span>
+
+        <button>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button type="button">Solicitar Reserva</button>
+      </footer>
     </div>
   );
 }
